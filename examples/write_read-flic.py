ab876ce8f3fa4b644c228bda3fe1bf01935b8925
tschudin <christian.tschudin@unibas.ch>
2018-01-20 16:06:04 +0100

    make flic a single file, other code cleanup

--- examples/write_read-flic.py
+++ examples/write_read-flic.py
@@ -11,13 +11,13 @@ import copy
 import random
 import sys
 sys.path.append('..')
-
-from PiCN.Packets.Name import Name
-from PiCN.Layers.PacketEncodingLayer.Encoder import NdnTlvEncoder
-from PiCN.ProgramLibs.Flic.content2flic import MkFlic
-from PiCN.ProgramLibs.Flic.flic2content import DeFlic
-import PiCN.client as client
 import time
+
+from PiCN                import client
+from PiCN.ProgramLibs    import flic
+from PiCN.Packets.Name   import Name
+from PiCN.Layers.PacketEncodingLayer.Encoder import NdnTlvEncoder
+import PiCN.client           as client
 
 # ---------------------------------------------------------------------------
 
@@ -28,31 +28,26 @@ if __name__ == '__main__':
     icn.attach("127.0.0.1", 9876, 9876, 'ndn2013')
 
     data = bytes(random.getrandbits(8) for _ in range(16*1024 + 123))
-    print("storing %d bytes" % len(data))
-    mkFlic = MkFlic(icn)
 
-    pfx = icn.getRepoPrefix()
-    name = copy.copy(pfx)
-    tstamp = time.strftime('%Y%m%d-%H%M%S') + '.txt'
-    name.__add__([tstamp.encode('ascii')])
+    # store
+    print("storing %d bytes via FLIC" % len(data))
+    tstamp = (time.strftime('%Y%m%d-%H%M%S') + '.txt').encode('ascii')
+    name = copy.copy(icn.getRepoPrefix()).__add__([tstamp])
+    name, _ = flic.MkFlic(icn).bytesToManifest(name, data)
 
-    name, rootManifest = mkFlic.bytesToManifest(name, data)
+    # retrieve
+    print("retrieving FLIC manifest '%s'" % name)
+    data2 = flic.DeFlic(icn).bytesFromManifestName(name)
 
-    deFlic = DeFlic(icn)
-    chunk = icn.readChunk(name)
-    content = NdnTlvEncoder().decode(chunk)
-    name._components.pop()
-    data2 = deFlic.manifestToBytes(name, content.get_bytes())
-
-    if data2:
-        print("retrieved content: %d bytes" % len(data2))
-        if data == data2:
-            print("  contents match!")
-        else:
-            print("  contents differ :-(")
-    else:
+    if not data2:
         print("ERROR: no content received")
         raise IOError
+
+    print("retrieved content: %d bytes" % len(data2))
+    if data == data2:
+        print("  contents match!")
+    else:
+        print("  contents differ :-(")
 
     icn.detach()
 
