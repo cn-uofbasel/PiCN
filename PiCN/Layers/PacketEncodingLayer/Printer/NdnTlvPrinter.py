f2133314f0f977a8636ec5e00489d3034230aaff
Claudio Marxer <marxer@claudio.li>
2018-01-29 15:03:46 +0100

    NdnTlvPrinter extension: Escape non-prinatable ascii characters

--- PiCN/Layers/PacketEncodingLayer/Printer/NdnTlvPrinter.py
+++ PiCN/Layers/PacketEncodingLayer/Printer/NdnTlvPrinter.py
@@ -161,6 +161,6 @@ class NdnTlvPrinter(object):
             NdnTlvPrinter.print_without_newline(NdnTlvPrinter.byte_to_hex(e))
             if idx % 8 == 0:
                 NdnTlvPrinter.print_without_newline("\t".expandtabs(50 - 3 * self.__indention_level))
-                NdnTlvPrinter.print_without_newline(self.__wire_format[self.__position-8 : self.__position].decode('ascii'))
+                NdnTlvPrinter.print_without_newline(self.__wire_format[self.__position-8 : self.__position].decode('ascii', 'replace'))
         NdnTlvPrinter.print_without_newline("\t".expandtabs(50 + (8 - idx % 8) * 3 - 3 * self.__indention_level))
-        NdnTlvPrinter.print_without_newline(self.__wire_format[self.__position - (idx % 8): self.__position].decode('ascii'))
+        NdnTlvPrinter.print_without_newline(self.__wire_format[self.__position - (idx % 8): self.__position].decode('ascii', 'replace'))
