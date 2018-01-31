78758f27265b2d90d79e06b6866b2aa02140af76
Claudio Marxer <marxer@claudio.li>
2018-01-31 11:34:24 +0100

    ICNForwarder: Refactor Executable (2)

--- PiCN/Executable/ICNForwarder.py
+++ PiCN/Executable/ICNForwarder.py
@@ -2,7 +2,6 @@
 
 import argparse
 import logging
-import datetime
 
 import PiCN.ProgramLibs.ICNForwarder
 from PiCN.Logger import Logger
@@ -18,18 +17,20 @@ def main(args):
         log_level = logging.WARNING
     elif args.logging == 'info':
         log_level = logging.INFO
-    else:
+    elif args.logging == 'debug':
         log_level = logging.DEBUG
+    else:
+        log_level = 255
     logger = Logger("ICNForwarder", log_level)
 
     # Info
     logger.info("Starting a CCN Forwarder...")
-    logger.info("UDP Port:    " + str(args.port))
-    logger.info("Log Level:   " + args.logging)
-    logger.info("Wire Format: " + args.suite)
+    logger.info("UDP Port:       " + str(args.port))
+    logger.info("Log Level:      " + args.logging)
+    logger.info("Packet Format:  " + args.format)
 
     # Packet encoder
-    encoder = NdnTlvEncoder() if args.suite == 'ndntlv' else SimpleStringEncoder
+    encoder = NdnTlvEncoder() if args.format == 'ndntlv' else SimpleStringEncoder
 
     # Start
     forwarder = PiCN.ProgramLibs.ICNForwarder.ICNForwarder(args.port, log_level, encoder)
@@ -38,9 +39,9 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='CCN Forwarder')
-    parser.add_argument('port', type=int, default=9000, help="UDP port (default: 9000)")
-    parser.add_argument('--suite', choices=['ndntlv','simple'], type=str, default='ndntlv', help='default is: "ndntlv"')
-    parser.add_argument('--logging', choices=['debug','info', 'warning', 'error'], type=str, default='info', help='default is: "debug"')
+    parser = argparse.ArgumentParser(description='PiCN Forwarder')
+    parser.add_argument('-p', '--port', type=int, default=9000, help="UDP port (default: 9000)")
+    parser.add_argument('-f', '--format', choices=['ndntlv','simple'], type=str, default='ndntlv', help='Packet Format (default: ndntlv)')
+    parser.add_argument('-l', '--logging', choices=['debug','info', 'warning', 'error', 'none'], type=str, default='info', help='Logging Level (default: info)')
     args = parser.parse_args()
     main(args)
