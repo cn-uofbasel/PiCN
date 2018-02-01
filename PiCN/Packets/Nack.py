44ff6d64bd0893627173c8054ae164e93d0b7033
Claudio Marxer <marxer@claudio.li>
2018-02-01 15:02:35 +0100

    Extension of PiCN.Packets.Nack: Interest property

--- PiCN/Packets/Nack.py
+++ PiCN/Packets/Nack.py
@@ -1,25 +1,36 @@
 """Internal representation of an NACK (negative acknowledgement) packet"""
 
 from .Packet import Packet
+from .Interest import Interest
 
 class Nack(Packet):
     """
     Internal representation of an NACK (negative acknowledgement) packet
     """
 
-    def __init__(self, name=None, wire_format=None, reason=None):
+    def __init__(self, name=None, wire_format=None, reason=None, interest=None):
         Packet.__init__(self, name)
         self._reason = reason
         self._wire_format = wire_format
+        self._interest = interest
         assert (type(self._wire_format) in [bytes, bytearray, type(None)]), "MUST be raw bytes or None"
+        assert (type(self._interest) in [Interest, type(None)]), "MUST be Interest or None"
 
     @property
     def reason(self):
         return self._reason
 
+    @property
+    def interest(selfs):
+        return selfs._interest
+
     @reason.setter
     def reason(self, reason):
         self._reason = reason
+
+    @interest.setter
+    def interest(self, i:Interest):
+        self._interest = i
 
     def __eq__(self, other):
         if type(other) is not Nack:
