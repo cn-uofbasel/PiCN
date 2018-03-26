1589986ff1d807ef272f9de92250798796a3b2a0
Claudio Marxer <marxer@claudio.li>
2018-03-26 13:56:55 +0200

    docs/config.py: try to enable sphinx-apidoc

--- docs/config.py
+++ docs/config.py
@@ -1,7 +1,19 @@
-from recommonmark.parser import CommonMarkParser
+# from recommonmark.parser import CommonMarkParser
 
-source_parsers = {
-    '.md': CommonMarkParser,
-}
+# source_parsers = {
+    # '.md': CommonMarkParser,
+# }
 
-source_suffix = ['.md']
+# source_suffix = ['.md']
+
+def run_apidoc(_):
+	from sphinx.apidoc import main
+	import os
+	import sys
+	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+	cur_dir = os.path.abspath(os.path.dirname(__file__))
+	module = os.path.join(cur_dir,"..","labbookdb")
+	main(['-e', '-o', cur_dir, module, '--force'])
+
+def setup(app):
+	app.connect('builder-inited', run_apidoc)
