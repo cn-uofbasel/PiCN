f0717a2431a7a4203304f7997999b91dafaf96dd
s3lph <5564491+s3lph@users.noreply.github.com>
2018-04-04 15:34:01 +0200

    Removed unconditional enabling of broadcast sockopt in UDP4LinkLayer

--- PiCN/Layers/LinkLayer/UDP4LinkLayer.py
+++ PiCN/Layers/LinkLayer/UDP4LinkLayer.py
@@ -24,7 +24,6 @@ class UDP4LinkLayer(LayerProcess):
 
         #Network data, used with a reference
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.sock.bind(("0.0.0.0", self._port))
 
     def get_port(self) -> int:
