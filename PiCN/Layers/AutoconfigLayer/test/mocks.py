5d277a5d15272f22bbcaa7af6d89b3d1357d896f
s3lph <5564491+s3lph@users.noreply.github.com>
2018-04-08 18:24:37 +0200

    Implemented unit tests for AutoconfigClientLayer

--- PiCN/Layers/AutoconfigLayer/test/mocks.py
+++ PiCN/Layers/AutoconfigLayer/test/mocks.py
@@ -20,8 +20,9 @@ class MockLinkLayer(UDP4LinkLayer):
     # noinspection PyMissingConstructor
     # super constructor not called, as no actual socket should be instantiated.
     def __init__(self, port: int = 9000):
+        manager = multiprocessing.Manager()
         self._id_gen = 0
-        self._ip_to_fid: Dict[Tuple[str, int], int] = {}
+        self._ip_to_fid: Dict[Tuple[str, int], int] = manager.dict()
         self._port = port
         self.sock = MockSocket()
         self.get_or_create_fid = unittest.mock.Mock(side_effect=self._get_or_create_fid)
