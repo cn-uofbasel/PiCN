8c183aa000231ad3fdb4c04bbf2391169086f600
Claudio Marxer <marxer@claudio.li>
2018-04-23 15:37:50 +0200

    Playground: Assisted Sharing: manifest and chunk generator

--- PiCN/Playground/AssistedSharing/RepoLayer.py
+++ PiCN/Playground/AssistedSharing/RepoLayer.py
@@ -1,9 +1,12 @@
 """Repository Layer"""
 
 import multiprocessing
+import mmap
+import math
+import hashlib
 
-from PiCN.Layers.ICNLayer.ContentStore.ContentStoreMemoryPrefix import ContentStoreMemoryPrefix
-from PiCN.Packets import Content, Interest, Packet, Nack, NackReason
+from PiCN.Layers.ICNLayer.ContentStore.ContentStoreMemoryExact import ContentStoreMemoryExact
+from PiCN.Packets import Content, Interest, Packet, Nack, NackReason, Name
 from PiCN.Processes import LayerProcess
 from PiCN.Playground.AssistedSharing.SampleData import alice_index_schema
 
@@ -15,9 +18,11 @@ class RepoLayer(LayerProcess):
         if manager is None:
             manager = multiprocessing.Manager()
         self._data_structs = manager.dict()
-        cs = ContentStoreMemoryPrefix()
+        cs = ContentStoreMemoryExact()
         cs.add_content_object(Content("/alice/schema.index", alice_index_schema))
         self._data_structs['cs'] = cs
+        self._files = {"/alice/movies/cats-and-dogs.mp4" : "/tmp/cats-and-dogs.mp4",
+                       "/alice/img/basel.jpg" : "/tmp/basel.jpg"}
 
 
     def data_from_higher(self, to_lower: multiprocessing.Queue, to_higher: multiprocessing.Queue, data):
@@ -52,17 +57,64 @@ class RepoLayer(LayerProcess):
 
 
     def handle_interest(self, face_id: int, interest: Interest, to_lower: multiprocessing.Queue, to_higher: multiprocessing.Queue, from_local: bool = False):
-        self.logger.info("Received Interest for " + interest.name.to_string())
-        cs_entry = self._data_structs['cs'].find_content_object(interest.name)
-        if cs_entry is not None:
-            self.logger.info("Found in database")
-            to_lower.put([face_id, cs_entry.content])
-            return
-        else:
-            self.logger.info("Not found in database")
-            nack = Nack(interest.name, NackReason.NO_CONTENT, interest=interest)
-            to_higher.put([face_id, nack])
 
+         cs_entry = self.cs.find_content_object(interest.name)
+         if cs_entry is not None:
+             self.logger.info("Found in cache")
+             to_lower.put([face_id, cs_entry.content])
+             return
+         else:
+             self.logger.info("Not found in cache, try to generate")
+             self.generate_data(interest.name)
+             cs_entry = self.cs.find_content_object(interest.name)
+             if cs_entry is not None:
+                 manifest = self.cs.find_content_object(interest.name).content
+                 to_lower.put([face_id, manifest])
+                 return
+             else:
+                  nack = Nack(interest.name, NackReason.NO_CONTENT, interest=interest)
+                  to_lower.put([face_id, nack])
+
+
+    def generate_data(self, network_name: Name):
+        fs_name = self._files[network_name.to_string()]
+        with open(fs_name, "r+") as f:
+            # open file and determine number of chunks
+            file = mmap.mmap(f.fileno(), 0)
+            file_length = len(file)
+            num_chunks = math.ceil(file_length / 4096) # chunk size: 4096 bytes
+            # generate data packets (manifest and chunk)
+            chunk_names = list()
+            for n in range(0, num_chunks-1):
+                # extract chunk and compute digest
+                chunk = file[n : min(n+1, file_length)]
+                m = hashlib.sha256()
+                m.update(chunk)
+                digest = m.hexdigest()
+                chunk_network_name = Name(network_name.to_string() + '/chunk/' + digest)
+                # add to cs and chunk list
+                chunk_names.append(chunk_network_name.to_string())
+                self.add_to_cs(Content(chunk_network_name, chunk))
+            # generate manifest
+            manifest_data = "\n".join(chunk_names)
+            manifest = Content(network_name, manifest_data)
+            self.add_to_cs(manifest)
+
+    @property
+    def cs(self):
+        """The Content Store"""
+        return self._data_structs.get('cs')
+
+    @cs.setter
+    def cs(self, cs):
+        self._data_structs['cs'] = cs
+
+    def add_to_cs(self, content: Content):
+        #cs = self._data_structs.get('cs')
+        cs = self.cs
+        cs.add_content_object(content)
+        # self._data_structs['cs'] = cs
+        self.cs = cs
 
     def ageing(self):
             pass
