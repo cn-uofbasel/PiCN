a865970112f76557e84be0cb89b05ce3f27173c5
Christopher Scherb <christopher.scherb@unibas.ch>
2018-06-26 23:05:15 +0200

    Add linklayer test sceleton

---
+++ PiCN/Layers/LinkLayer/test/test_BasicLinkLayer.py
@@ -0,0 +1,13 @@
+"""Test the Basic Link Layer"""
+
+import unittest
+
+
+class test_BasicLinkLayer(unittest.TestCase):
+    """Test the Basic Link Layer"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
