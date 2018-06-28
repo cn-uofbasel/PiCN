83711fcb02d8c637224e736c22177b3b313a2c99
Claudio Marxer <marxer@claudio.li>
2018-06-28 19:36:25 +0200

    Playground: PinnedNFN (dima): add simple service (pow)

--- PiCN/Playground/PinnedNFN/PinnedComputationLayer.py
+++ PiCN/Playground/PinnedNFN/PinnedComputationLayer.py
@@ -1,4 +1,5 @@
 import multiprocessing
+from math import pow
 
 from PiCN.Processes import LayerProcess
 from PiCN.Packets import Interest, Content
@@ -23,17 +24,34 @@ class PinnedComputationLayer(LayerProcess):
             self.logger.info("Received packet is not an interest")
             return
 
+    def return_result(self, packet_id, content: Content):
+        self.queue_to_lower.put([packet_id, content])
+
     def handleInterest(self, packet_id: int, interest: Interest):
         components = interest.name.components
         self.logger.info(components[-1])
         if components[-1] == b"pNFN":
             num_params = int(components[-2]) # TODO -- error handling
             params = components[-num_params-2:-2] # TODO -- error handling
+            params = list(map(lambda x: x.decode('utf-8'), params))
             function_name = components[:-num_params-2]
-            # TODO -- execute according to function_name and params
+            function_name = "/" + "/".join(list(map(lambda x: x.decode('utf-8'), function_name)))
+            if function_name == "/the/prefix/square":
+                result = self.pinned_function_square(params)
+                self.return_result(packet_id, Content(interest.name, str(result))) # QUESTION -- return as string?
+                self.logger.info("Result returned")
+                return
+            else:
+                self.logger.info("Pinned function not available.")
+                # TODO -- return NACK
+
         else:
             self.logger.info("Received interest does not contain a computation expression")
         return
+
+    def pinned_function_square(self, params):
+        # TODO -- check if params contains valid parameters
+        return pow(int(params[0]), 2)
 
     def ageing(self):
         pass  # ageing not necessary
