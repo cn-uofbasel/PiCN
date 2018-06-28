4dfe9cec829bf5258a3cf1af4189b734c75fd50d
Claudio Marxer <marxer@claudio.li>
2018-06-28 13:14:45 +0200

    Playground: PinnedNFN (dima): fix issue with PIT

--- PiCN/Playground/PinnedNFN/PinnedNFNStack.py
+++ PiCN/Playground/PinnedNFN/PinnedNFNStack.py
@@ -8,7 +8,7 @@ from PiCN.Layers.ICNLayer import BasicICNLayer
 from PiCN.Layers.PacketEncodingLayer.Encoder import BasicEncoder, NdnTlvEncoder
 from PiCN.Processes import PiCNSyncDataStructFactory
 
-from PiCN.Layers.ICNLayer.PendingInterestTable import PendingInterestTableMemoryExact
+from PiCN.Layers.ICNLayer.PendingInterestTable import BasePendingInterestTable
 from PiCN.Layers.ICNLayer.ContentStore import ContentStoreMemoryExact
 from PiCN.Layers.ICNLayer.ForwardingInformationBase import ForwardingInformationBaseMemoryPrefix
 
@@ -30,7 +30,7 @@ class PinnedNFNStack(object):
         synced_data_struct_factory = PiCNSyncDataStructFactory()
         synced_data_struct_factory.register("cs", ContentStoreMemoryExact)
         synced_data_struct_factory.register("fib", ForwardingInformationBaseMemoryPrefix)
-        synced_data_struct_factory.register("pit", PendingInterestTableMemoryExact)
+        synced_data_struct_factory.register("pit", BasePendingInterestTable)
         synced_data_struct_factory.register("face_id_table", FaceIDDict)
         synced_data_struct_factory.create_manager()
 
