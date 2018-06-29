7ec14f7860959bed3fc540d694cbf9c0fc70b785
Claudio Marxer <marxer@claudio.li>
2018-06-29 11:28:43 +0200

    ContentStorePersistentExact

---
+++ PiCN/Layers/ICNLayer/ContentStore/ContentStorePersistentExact.py
@@ -0,0 +1,48 @@
+""" An persistent content store with exact matching"""
+
+import time
+import shelve
+
+from PiCN.Packets import Content, Name
+from PiCN.Layers.ICNLayer.ContentStore import BaseContentStore, ContentStoreEntry
+
+
+class ContentStorePersistentExact(BaseContentStore):
+    """ A persistent content store with exact matching"""
+
+    def __init__(self, cs_timeout: int = 10):
+        self.db_path = "/tmp/database.db"  # TODO
+        self._container = shelve.open(self.db_path)
+        self._cs_timeout = cs_timeout
+
+    def close(self):
+        self._container.clear()
+
+    def find_content_object(self, name: Name) -> ContentStoreEntry:
+        if name.to_string() in self._container.keys():
+            return self._container[name.to_string()]
+        else:
+            return None
+
+    def add_content_object(self, content: Content, static: bool = False):
+        self._container[content.name.to_string()] = ContentStoreEntry(content, static=static)
+
+    def remove_content_object(self, name: Name):
+        if name.to_string() in self._container.keys():
+            del self._container[name.to_string()]
+
+    def update_timestamp(self, cs_entry: ContentStoreEntry):
+        self.remove_content_object(cs_entry.name)
+        cs_entry.timestamp = time.time()
+        self._container[cs_entry.name.to_string()] = cs_entry
+
+    def ageing(self):
+        keys_to_remove = []
+        cur_time = time.time()
+        for key in self._container:
+            if self._container[key].static is True:
+                continue
+            if self._container[key].timestamp + self._cs_timeout < cur_time:
+                keys_to_remove.append(key)
+        for key in keys_to_remove:
+            self.remove_content_object(self._container[key].name.to_string())
