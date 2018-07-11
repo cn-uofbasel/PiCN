c34072909c04dd113430c5dcf0336fef9c020f46
Claudio Marxer <marxer@claudio.li>
2018-07-11 19:50:34 +0200

    Starters: Pinned NFN: clean up

--- PiCN/Playground/PinnedNFN/PinnedComputationLayer.py
+++ PiCN/Playground/PinnedNFN/PinnedComputationLayer.py
@@ -1,13 +1,13 @@
 import multiprocessing
 import threading
-from math import pow
 import time
+from math import pow
+
 from PiCN.Processes import LayerProcess
 from PiCN.Packets import Name, Interest, Content, Nack, NackReason
-import threading
+
 
 class PinnedComputationLayer(LayerProcess):
-
     def __init__(self, replica_id, log_level=255):
         super().__init__(logger_name="PinnedNFNLayer (" + str(replica_id) + ")", log_level=log_level)
         self.storage = None
@@ -30,9 +30,10 @@ class PinnedComputationLayer(LayerProcess):
         self.queue_to_lower.put([packet_id, content])
 
     def return_nack(self, packet_id, interest: Interest):
-        self.queue_to_lower.put([packet_id, Nack(interest.name, reason=NackReason.NOT_SET, interest=interest)])  # TODO -- choose an appropriate NACK reason
+        self.queue_to_lower.put([packet_id, Nack(interest.name, reason=NackReason.NOT_SET,
+                                                 interest=interest)])  # TODO -- choose an appropriate NACK reason
 
-    def handleInterest (self, packet_id: int, interest: Interest):
+    def handleInterest(self, packet_id: int, interest: Interest):
 
         components = interest.name.components
         if components[-1] == b"pNFN":
@@ -40,7 +41,7 @@ class PinnedComputationLayer(LayerProcess):
                 num_params = int(components[-2])
                 self.params = components[-num_params - 2:-2]
                 self.params = list(map(lambda x: x.decode('utf-8'), self.params))
-                assert(num_params < len(interest.name.components)-2)
+                assert (num_params < len(interest.name.components) - 2)
                 self.function_name = components[:-num_params - 2]
                 self.function_name = "/" + "/".join(list(map(lambda x: x.decode('utf-8'), self.function_name)))
 
@@ -52,58 +53,20 @@ class PinnedComputationLayer(LayerProcess):
             if self.function_name == "/the/prefix/square":
                 self.return_result(packet_id, Content(interest.name, "5"))  # QUESTION -- return as string?
                 self.logger.info("Result returned")
-                #self.result = self.pinned_function_square(self.params)
-                #run the computation in a thread
                 arguments = [self.pinned_function_square, self.params, interest.name]
                 t = threading.Thread(target=self.executePinnedFunction, args=arguments)
                 t.setDaemon(True)
                 t.start()
                 return
             else:
-               self.return_nack(packet_id, interest)
-               self.logger.info("Pinned function not available. Return NACK.")
+                self.return_nack(packet_id, interest)
+                self.logger.info("Pinned function not available. Return NACK.")
             return
-        if components[-1] == b"resultpNFN":
-            try:
-                num_parameters = int(components[-2])
-                parameters = components[-num_parameters - 2:-2]
-                parameters = list(map(lambda x: x.decode('utf-8'), parameters))
-                assert(num_parameters < len(interest.name.components)-2)
-                func_name = components[:-num_parameters - 2]
-                func_name = "/" + "/".join(list(map(lambda x: x.decode('utf-8'), func_name)))
-
-            except:
-                self.return_nack(packet_id, interest)
-                self.logger.info("Invalid computation expression. Return NACK.")
-                return
-
-
-            if func_name == self.function_name and parameters == self.params:
-                self.return_result(packet_id, Content(interest.name, "empty"))
-                return
-            #if function_name == "/the/prefix/squareresult" and parameters == self.params:
-               # self.return_result(packet_id, Content(interest.name, str(self.result)))
-                #return
-            else:
-                self.return_nack(packet_id, interest)
-                self.logger.info("Pinned function is not computed yet. Return NACK.")
-                return
-
-        # elif components[-1] == b"resultpNFN":
-        #     try:
-        #         func_name = components[:-1]
-        #         func_name = "/" + "/".join(list(map(lambda x: x.decode('utf-8'), func_name)))
-        #     except:
-        #         self.return_nack(packet_id, interest)
-        #         self.logger.info("Invalid computation expression. Return NACK.")
-        #         return
-
-
         else:
             self.logger.info("Received interest does not contain a computation expression")
         return
 
-    # defining some pinned functions
+    ### defining some pinned functions
 
     def executePinnedFunction(self, function, params, interest_name: Name):
         result = function(params)
@@ -113,7 +76,7 @@ class PinnedComputationLayer(LayerProcess):
         content_object = Content(new_name, str(result))
         self.queue_to_lower.put([-1, content_object])
 
-    def pinned_function_square(self,params):
+    def pinned_function_square(self, params):
         # TODO -- check if params contains valid parameters
         time.sleep(5)
         return int(pow(int(params[0]), 2))
