bf4f64932dad0e4555726404da6f2dfa5c3438b8
Christopher Scherb <christopher.scherb@unibas.ch>
2018-08-20 23:03:31 +0200

    test for timeout prevention

--- PiCN/Layers/TimeoutPreventionLayer/BasicTimeoutPreventionLayer.py
+++ PiCN/Layers/TimeoutPreventionLayer/BasicTimeoutPreventionLayer.py
@@ -2,7 +2,7 @@
 Moreover, it contains handler for incomming R2C messages"""
 
 import multiprocessing
-import time
+import time, threading
 
 from typing import Dict
 
@@ -49,9 +49,11 @@ class TimeoutPreventionMessageDict(object):
         """
         entry = self.container.get(name)
         if entry is not None:
-            del self.container[name]
-        entry.timestamp = time.time()
-        self.container[name] = entry
+            self.remove_entry(name)
+        else:
+            return
+        entry_n = TimeoutPreventionMessageDict.TimeoutPreventionMessageDictEntry()
+        self.add_entry(name, entry_n)
 
     def remove_entry(self, name):
         """Remove an entry from the dict
@@ -60,36 +62,85 @@ class TimeoutPreventionMessageDict(object):
         if name in self.container:
             del self.container[name]
 
+    def get_container(self):
+        return self.container
+
 class BasicTimeoutPreventionLayer(LayerProcess):
     """BasicR2CLayer maintains a list of messages for which R2C messages should be sent.
     Moreover, it contains handler for incomming R2C messages"""
 
     def __init__(self, message_dict: TimeoutPreventionMessageDict):
+        self.timeout_interval = 2
+        self.ageing_interval = 1
         self.message_dict = message_dict
 
     def data_from_lower(self, to_lower: multiprocessing.Queue, to_higher: multiprocessing.Queue, data):
         packet_id = data[0]
         packet = data[1]
         if isinstance(packet, Interest):
+            #TODO R2C request case, use compute table
             to_higher.put(data)
-        elif isinstance(packet, Content) and len(packet.name.components) > 2 and packet.name.string_components[-2] == 'R2C':
+        elif isinstance(packet, Content) and len(packet.name.components) > 2 and packet.name.string_components[-2] == 'KEEPALIVE':
             self.message_dict.update_timestamp(packet.name) #update timestamp for the R2C message
             return
         elif isinstance(packet, Content) or isinstance(packet, Nack): #R2C Content or Nack, remove entry and give to higher layer
             entry = self.message_dict.get_entry(packet.name)
-            if entry is None:
-                return
-            self.message_dict.remove_entry(packet.name)
+            if entry is not None:
+                self.message_dict.remove_entry(packet.name)
+                keepalive_name = self.add_keep_alive_from_name(packet.name)
+                self.message_dict.remove_entry(keepalive_name)
             to_higher.put(data)
 
     def data_from_higher(self, to_lower: multiprocessing.Queue, to_higher: multiprocessing.Queue, data):
         packet_id = data[0]
         packet = data[1]
 
-        if isinstance(packet, Interest) and packet.name.components[-1] == "NFN":
+        if isinstance(packet, Interest) and packet.name.string_components[-1] == "NFN":
+            keepalive_name = self.add_keep_alive_from_name(packet.name)
             self.message_dict.create_entry(name=packet.name)
+            self.message_dict.create_entry(name=keepalive_name)
         to_lower.put(data)
 
     def ageing(self):
-        #todo
-        pass
+        try:
+            removes = []
+            container = self.message_dict.get_container()
+            for name in container:
+                entry = self.message_dict.get_entry(name)
+                if len(name.components) > 2 and name.string_components[-2] == "KEEPALIVE":
+                    if entry.timestamp + self.timeout_interval < time.time():
+                        removes.append(name)
+                        original_name = self.remove_keeep_alive_from_name(name)
+                        removes.append(original_name)
+                        nack = Nack(name=original_name, reason=NackReason.NOT_SET, interest=Interest(name))
+                        self.queue_to_higher.put([-1, nack]) #TODO ID
+                    else:
+                        self.queue_to_lower.put([-1, Interest(name=name)])
+                else:
+                    self.queue_to_lower.put([-1, Interest(name=name)])
+            for n in removes:
+                self.message_dict.remove_entry(n)
+        finally:
+            t = threading.Timer(self.ageing_interval, self.ageing)
+            t.setDaemon(True)
+            t.start()
+
+    def add_keep_alive_from_name(self, name):
+        if name.components[-1] != b"NFN":
+            return name
+        new_name = Name()
+        for n in name.string_components:
+            new_name += n
+        new_name.components.remove(b"NFN")
+        new_name += "KEEPALIVE"
+        new_name += "NFN"
+        return new_name
+
+    def remove_keeep_alive_from_name(self, name):
+        if name.components[-1] != b"NFN":
+            return name
+        new_name = Name()
+        for n in name.string_components:
+            new_name += n
+        new_name.components.remove(b"KEEPALIVE")
+        return new_name
--- PiCN/Layers/TimeoutPreventionLayer/test/test_BasicTimeoutPreventionLayer.py
+++ PiCN/Layers/TimeoutPreventionLayer/test/test_BasicTimeoutPreventionLayer.py
@@ -1,10 +1,11 @@
 """Tests of the BasicR2CLayer"""
 
+import time
 import unittest
 import multiprocessing
 
 from PiCN.Layers.TimeoutPreventionLayer import BasicTimeoutPreventionLayer, TimeoutPreventionMessageDict
-from PiCN.Packets import Interest
+from PiCN.Packets import Interest, Content, Nack, NackReason
 from PiCN.Processes import PiCNSyncDataStructFactory
 
 class test_BasicTimeoutPreventionLayer(unittest.TestCase):
@@ -32,5 +33,221 @@ class test_BasicTimeoutPreventionLayer(unittest.TestCase):
         """test that an interest from lower is directly forwarded to upper"""
         interest =  Interest("/test/data")
         self.timeoutPreventionLayer.queue_from_lower.put([1, interest])
-        res = self.timeoutPreventionLayer.queue_to_higher.get()
+        res = self.timeoutPreventionLayer.queue_to_higher.get(timeout=2.0)
         self.assertEqual([1, interest], res)
+
+    def test_interest_from_higher(self):
+        """test sending an interest from higher without adding it to the dict"""
+        interest = Interest("/test/data")
+        self.timeoutPreventionLayer.queue_from_higher.put([1, interest])
+        res = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual([1, interest], res)
+        e = self.timeoutPreventionLayer.message_dict.get_entry(interest.name)
+        self.assertTrue(e is None)
+
+    def test_nfn_interest_from_higher(self):
+        """test sending an interest from higher and adding it to the dict"""
+        interest = Interest("/test/func()/NFN")
+        self.timeoutPreventionLayer.queue_from_higher.put([1, interest])
+        res = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual([1, interest], res)
+        e = self.timeoutPreventionLayer.message_dict.get_entry(interest.name)
+        self.assertTrue(e is not None)
+
+    def test_content_from_lower_no_message_dict_entry(self):
+        """test content from lower with no message dict entry"""
+        content = Content("/test/data")
+        self.timeoutPreventionLayer.queue_from_lower.put([1, content])
+        res = self.timeoutPreventionLayer.queue_to_higher.get(timeout=2.0)
+        self.assertEqual([1, content], res)
+        e = self.timeoutPreventionLayer.message_dict.get_entry(content.name)
+        self.assertTrue(e is None)
+
+    def test_content_from_lower_message_dict_entry(self):
+        """test content from lower with message dict entry"""
+        content = Content("/test/data")
+        self.timeoutPreventionLayer.message_dict.create_entry(content.name)
+        e = self.timeoutPreventionLayer.message_dict.get_entry(content.name)
+        self.assertTrue(e is not None)
+        self.timeoutPreventionLayer.queue_from_lower.put([1, content])
+        res = self.timeoutPreventionLayer.queue_to_higher.get(timeout=2.0)
+        self.assertEqual([1, content], res)
+        e = self.timeoutPreventionLayer.message_dict.get_entry(content.name)
+        self.assertTrue(e is None)
+
+    def test_content_from_higher(self):
+        """test content from higher"""
+        content = Content("/test/data")
+        self.timeoutPreventionLayer.queue_from_higher.put([1, content])
+        res = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual([1, content], res)
+
+    def test_nack_from_lower_message_dict_entry(self):
+        """test nack from lower WITH message dict entry"""
+        nack = Nack("/test/data", interest=Interest("/test/data"), reason=NackReason.CONGESTION)
+        self.timeoutPreventionLayer.message_dict.create_entry(nack.name)
+        e = self.timeoutPreventionLayer.message_dict.get_entry(nack.name)
+        self.assertTrue(e is not None)
+        self.timeoutPreventionLayer.queue_from_lower.put([1, nack])
+        res = self.timeoutPreventionLayer.queue_to_higher.get(timeout=2.0)
+        self.assertEqual([1, nack], res)
+        e = self.timeoutPreventionLayer.message_dict.get_entry(nack.name)
+        self.assertTrue(e is None)
+
+    def test_nack_from_higher(self):
+        """test nack from higher """
+        nack = Nack("/test/data", interest=Interest("/test/data"), reason=NackReason.CONGESTION)
+        self.timeoutPreventionLayer.queue_from_higher.put([1, nack])
+        res = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual([1, nack], res)
+
+    def test_keepalive_from_lower_message_dict_entry(self):
+        """test r2c from lower with message dict entry"""
+        content = Content("/test/data/_()/KEEPALIVE/NFN")
+        self.timeoutPreventionLayer.message_dict.create_entry(content.name)
+        e1 = self.timeoutPreventionLayer.message_dict.get_entry(content.name)
+        ts = e1.timestamp
+        self.assertTrue(e1 is not None)
+        self.timeoutPreventionLayer.queue_from_lower.put([1, content])
+        time.sleep(1.0)
+        e2 = self.timeoutPreventionLayer.message_dict.get_entry(content.name)
+        self.assertTrue(e2 is not None)
+        self.assertTrue(e2.timestamp > ts)
+
+    def test_keep_alive_ageing_no_reply(self):
+        """test ageing with keepalive with no keep alive reply"""
+        self.timeoutPreventionLayer.ageing()
+
+        interest = Interest("/test/func/_()/NFN")
+        keepalive = Interest("/test/func/_()/KEEPALIVE/NFN")
+        self.timeoutPreventionLayer.queue_from_higher.put([1, interest])
+
+        res1 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res1, [1, interest])
+        e1 = self.timeoutPreventionLayer.message_dict.get_entry(interest.name)
+        self.assertTrue(e1 is not None)
+        e2 = self.timeoutPreventionLayer.message_dict.get_entry(keepalive.name)
+        self.assertTrue(e2 is not None)
+
+        res2 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res2, [-1, interest])
+
+        time.sleep(1)
+
+        res3 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res3, [-1, keepalive])
+
+        time.sleep(1)
+
+        res4 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res4, [-1, interest])
+
+        time.sleep(1)
+
+        res5 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res5, [-1, keepalive])
+
+        time.sleep(1)
+
+        res6 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res6, [-1, interest])
+
+        self.assertTrue(self.timeoutPreventionLayer.queue_to_lower.empty())
+
+        res7 = self.timeoutPreventionLayer.queue_to_higher.get(timeout=2.0)
+        self.assertEqual(res7, [-1, Nack(name=interest.name, reason=NackReason.NOT_SET, interest=interest)])
+
+    def test_keep_alive_ageing_reply(self):
+        """test ageing with keepalive with no keep alive reply"""
+        self.timeoutPreventionLayer.ageing()
+
+        interest = Interest("/test/func/_()/NFN")
+        content = Content(interest.name, "data")
+        keepalive = Interest("/test/func/_()/KEEPALIVE/NFN")
+        keepalive_rely = Content(keepalive.name, "")
+
+        self.timeoutPreventionLayer.queue_from_higher.put([1, interest])
+
+        res1 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res1, [1, interest])
+        e1 = self.timeoutPreventionLayer.message_dict.get_entry(interest.name)
+        self.assertTrue(e1 is not None)
+        e2 = self.timeoutPreventionLayer.message_dict.get_entry(keepalive.name)
+        self.assertTrue(e2 is not None)
+
+        res2 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res2, [-1, interest])
+
+        time.sleep(1)
+
+        res3 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res3, [-1, keepalive])
+
+        time.sleep(1)
+
+        self.timeoutPreventionLayer.queue_from_lower.put([4, keepalive_rely])
+        e1 = self.timeoutPreventionLayer.message_dict.get_entry(interest.name)
+        self.assertTrue(e1 is not None)
+        e2 = self.timeoutPreventionLayer.message_dict.get_entry(keepalive.name)
+        self.assertTrue(e2 is not None)
+
+        res4 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res4, [-1, interest])
+
+        time.sleep(1)
+
+        res5 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res5, [-1, keepalive])
+
+        time.sleep(1)
+
+        self.timeoutPreventionLayer.queue_from_lower.put([4, keepalive_rely])
+        e1 = self.timeoutPreventionLayer.message_dict.get_entry(interest.name)
+        self.assertTrue(e1 is not None)
+        e2 = self.timeoutPreventionLayer.message_dict.get_entry(keepalive.name)
+        self.assertTrue(e2 is not None)
+
+        time.sleep(1)
+
+        res6 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res6, [-1, interest])
+
+        time.sleep(1)
+
+        res7 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res7, [-1, keepalive])
+
+        self.timeoutPreventionLayer.queue_from_lower.put([4, keepalive_rely])
+        time.sleep(1)
+
+        res8 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res8, [-1, interest])
+
+        time.sleep(1)
+
+        res9 = self.timeoutPreventionLayer.queue_to_lower.get(timeout=2.0)
+        self.assertEqual(res9, [-1, keepalive])
+
+        self.timeoutPreventionLayer.queue_from_lower.put([5, content])
+
+        time.sleep(1)
+
+        e1 = self.timeoutPreventionLayer.message_dict.get_entry(interest.name)
+        self.assertTrue(e1 is None)
+        e2 = self.timeoutPreventionLayer.message_dict.get_entry(keepalive.name)
+        self.assertTrue(e2 is None)
+        self.assertEqual(len(self.timeoutPreventionLayer.message_dict.get_container()), 0)
+
+        res = self.timeoutPreventionLayer.queue_to_higher.get(timeout=2.0)
+        self.assertEqual(res, [5, content])
+
+
+    def test_keep_alive_request(self):
+        """test replying a incoming keep alive request"""
+        #TODO
+        pass
+
+    def test_keep_alive_request_no_comp_running(self):
+        """test replying a incoming keep alive request if no comp running"""
+        #TODO
+        pass
