883f78d68c98e5a81937bb799c75282391290253
Christopher Scherb <christopher.scherb@unibas.ch>
2018-10-25 14:18:24 +0200

    remove timeout prevention layer for demo

--- PiCN/ProgramLibs/NFNForwarder/NFNForwarder.py
+++ PiCN/ProgramLibs/NFNForwarder/NFNForwarder.py
@@ -112,7 +112,7 @@ class NFNForwarder(object):
         # start processes
         self.lstack.start_all()
         self.icnlayer.ageing()
-        self.timeoutpreventionlayer.ageing()
+        #self.timeoutpreventionlayer.ageing()
         self.mgmt.start_process()
 
     def stop_forwarder(self):
