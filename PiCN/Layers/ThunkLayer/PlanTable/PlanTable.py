0ba63a30b69e751a810c4146dba5daafe91a24f0
Christopher Scherb <christopher.scherb@unibas.ch>
2018-11-16 00:02:42 +0100

    add plan table test for fwd

--- PiCN/Layers/ThunkLayer/PlanTable/PlanTable.py
+++ PiCN/Layers/ThunkLayer/PlanTable/PlanTable.py
@@ -38,11 +38,10 @@ class PlanTable(object):
         if plan == None:
             for e in self.container: #check for subcomp
                 entry_names = self.container.get(e)[0]
-                if len(entry_names) == 1:
-                    n1 = self.parser.network_name_to_nfn_str(entry_names[0])
-                    n2 = self.parser.network_name_to_nfn_str(name)
-                    if n1 == n2:
-                        return True
+                entry_names_str = list(map(lambda x: self.parser.network_name_to_nfn_str(x)[0],entry_names))
+                name_str = self.parser.network_name_to_nfn_str(name)[0]
+                if name_str in entry_names_str:
+                    return True
             return False
         elif len(plan) == 1:
             return True
--- PiCN/Layers/ThunkLayer/PlanTable/test/test_PlanTable.py
+++ PiCN/Layers/ThunkLayer/PlanTable/test/test_PlanTable.py
@@ -50,3 +50,58 @@ class test_PlanTable(unittest.TestCase):
         self.planTable.add_plan(name, requests, cost)
         res = self.planTable.get_cost(Name("/test/data2"))
         self.assertIsNone(res)
+
+    def test_compute_fwd(self):
+        """Test if computation forward is correct"""
+        name1 = Name("/test/data/d3")
+        name1 += "/func/f1(_)"
+        name1 += "NFN"
+        name2 = Name("/func/f1")
+        name2 += "_(/test/data/d3)"
+        name2 += "NFN"
+        self.planTable.add_plan(name1, [name2], 5)
+        res1 = self.planTable.compute_fwd(name1)
+        self.assertTrue(res1)
+        name3 = Name("/func/f1")
+        name3 += "_(/test/data/d4)"
+        name3 += "NFN"
+        res2 = self.planTable.compute_fwd(name3)
+        self.assertFalse(res2)
+
+    def test_compute_fwd_subcomps(self):
+        """Test if computation forward is correct for subcomps"""
+        name1 = Name("/test/data/d1")
+        name2 = Name("/hello/world/d2")
+        name3 = Name("/test/data/d3")
+        name3 += "/func/f1(_)"
+        name3 += "NFN"
+        self.planTable.add_plan(name1, [name2, name3], 5)
+
+        res1 = self.planTable.compute_fwd(name1)
+        self.assertFalse(res1)
+        res2 = self.planTable.compute_fwd(name2)
+        self.assertTrue(res2)
+        res3 = self.planTable.compute_fwd(name3)
+        self.assertTrue(res3)
+
+        name4 = Name("/func/f1")
+        name4 += "_(/test/data/d3)"
+        name4 += "NFN"
+
+        res4 = self.planTable.compute_fwd(name4)
+        self.assertTrue(res4)
+
+        name5 = Name("/func/f1")
+        name5 += "_(/test/data/d4)"
+        name5 += "NFN"
+
+        res5 = self.planTable.compute_fwd(name5)
+        self.assertFalse(res5)
+
+    def test_rewrite(self):
+        """Test rewrites"""
+        pass
+
+    def test_rewrite_subcomp(self):
+        """Test rewrites for subcomp"""
+        pass
