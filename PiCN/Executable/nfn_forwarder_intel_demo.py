b5c29451033666c7e148fd5816da001e33460825
Christopher Scherb <christopher.scherb@unibas.ch>
2018-12-07 16:00:22 +0100

    catch all exception

---
+++ PiCN/Executable/nfn_forwarder_intel_demo.py
@@ -0,0 +1,37 @@
+"""NFN Forwarder executable"""
+
+import logging
+import sys
+import argparse
+import time
+
+import PiCN.ProgramLibs.NFNForwarder
+from PiCN.Logger import Logger
+from PiCN.Layers.NFNLayer.NFNOptimizer import EdgeComputingOptimizer, MapReduceOptimizer
+from PiCN.Layers.PacketEncodingLayer.Encoder import SimpleStringEncoder, NdnTlvEncoder
+from PiCN.Layers.LinkLayer.Interfaces import AddressInfo
+from PiCN.Packets import *
+
+def main(argv):
+
+    # Log Level
+    log_level = logging.DEBUG
+
+    # Packet encoder
+    encoder = NdnTlvEncoder(log_level)
+    forwarder = PiCN.ProgramLibs.NFNForwarder.NFNForwarder(0, log_level, encoder)
+    forwarder.start_forwarder()
+
+    time.sleep(1)
+
+    add_info1 = AddressInfo(("127.0.0.1", 9100), 0)
+    fid1 = forwarder.linklayer.faceidtable.get_or_create_faceid(add_info1)
+    forwarder.icnlayer.fib.add_fib_entry(Name("/fct"), [fid1], True)
+
+    add_info2 = AddressInfo(("127.0.0.1", 9100), 0)
+    fid2 = forwarder.linklayer.faceidtable.get_or_create_faceid(add_info2)
+    forwarder.icnlayer.fib.add_fib_entry(Name("/dat"), [fid2], True)
+
+if __name__ == "__main__":
+    main(sys.argv)
+
