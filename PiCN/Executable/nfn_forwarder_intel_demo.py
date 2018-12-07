43c704272441b36a1c559160e0492da6f1c1e9b4
Christopher Scherb <christopher.scherb@unibas.ch>
2018-12-07 16:10:56 +0100

    intel demo upstart

--- PiCN/Executable/nfn_forwarder_intel_demo.py
+++ PiCN/Executable/nfn_forwarder_intel_demo.py
@@ -26,11 +26,11 @@ def main(argv):
 
     add_info1 = AddressInfo(("127.0.0.1", 9100), 0)
     fid1 = forwarder.linklayer.faceidtable.get_or_create_faceid(add_info1)
-    forwarder.icnlayer.fib.add_fib_entry(Name("/dat"), [fid1], True)
+    forwarder.icnlayer.fib.add_fib_entry(Name("/ndn/ch/unibas/nfn/dat"), [fid1], True)
 
     add_info2 = AddressInfo(("127.0.0.1", 9200), 0)
     fid2 = forwarder.linklayer.faceidtable.get_or_create_faceid(add_info2)
-    forwarder.icnlayer.fib.add_fib_entry(Name("/fct"), [fid2], True)
+    forwarder.icnlayer.fib.add_fib_entry(Name("/ndn/ch/unibas/nfn/fct"), [fid2], True)
 
 if __name__ == "__main__":
     main(sys.argv)
