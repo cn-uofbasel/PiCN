852e2dc6a89427fb41a218391c0f82acda8d29d5
da Costa Eric - coseri00 <eric.dacosta@stud.unibas.ch>
2020-03-04 22:10:38 +0100

    Update checkStreaming function to parse names correctly

--- PiCN/Layers/NFNLayer/NFNExecutor/NFNPythonExecutorStreaming.py
+++ PiCN/Layers/NFNLayer/NFNExecutor/NFNPythonExecutorStreaming.py
@@ -5,20 +5,25 @@ from PiCN.Layers.NFNLayer.NFNExecutor import NFNPythonExecutor
 class NFNPythonExecutorStreaming(NFNPythonExecutor):
 
     def __init__(self):
-        self._sandbox = super._init_sandbox()
+        self._sandbox = NFNPythonExecutor()._init_sandbox()
         self._sandbox["checkStreaming"] = self.checkStreaming
 
     def checkStreaming(self, arg: str):
         if not arg:
             print("String is empty")
-        elif arg[0:4] == "sdo:":
+        elif arg.startswith("sdo:"):
             print("File for streaming")
-            if (self.checkForName(arg[5:])):
-                print("File contains name")
+            tmpList = arg.splitlines()
+            tmpList.pop(0)
+            for x in tmpList:
+                if (self.checkForName(x)):
+                    print(x + " : is a name")
+                else:
+                    print("This entry is not a name")
         else:
             print("File not for streaming")
 
-    def checkForName(self, name: str):
+    def checkForName(name: str):
         if name[0] != "/":
             return False
         else:
