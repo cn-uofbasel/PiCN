e420a672396c9dd3332278bdb2e9a54d6808ba0d
dacostaeric <eric.dacosta@stud.unibas.ch>
2020-05-18 15:59:53 +0200

    Add bigger timeout to MultiNameSimulation

--- PiCN/Simulations/Streaming/MultiNameSimulation.py
+++ PiCN/Simulations/Streaming/MultiNameSimulation.py
@@ -48,6 +48,8 @@ nfn_fwd1 = NFNForwarder(port=0, encoder=NdnTlvEncoder(),
                         interfaces=[simulation_bus.add_interface("nfn1")], log_level=255, executors={"PYTHONSTREAM": NFNPythonExecutorStreaming()},
                         ageing_interval=1)
 nfn_fwd1.executors["PYTHONSTREAM"].initialize_executor(nfn_fwd1.nfnlayer.queue_to_lower, nfn_fwd1.nfnlayer.queue_from_lower, nfn_fwd1.nfnlayer.computation_table, nfn_fwd1.nfnlayer.cs, nfn_fwd1.icnlayer.pit)
+nfn_fwd0.icnlayer.pit.set_pit_timeout(20000)
+nfn_fwd1.icnlayer.pit.set_pit_timeout(20000)
 
 repo = ICNDataRepository("./InputFiles", Name("/repo/r1"), 0, 255, NdnTlvEncoder(), False, False, interfaces=[simulation_bus.add_interface("repo")])
 repo.start_repo()
@@ -68,14 +70,14 @@ mgmt_client0.add_forwarding_rule(Name("/lib"), [0])
 mgmt_client1.add_face("repo", None, 0)
 mgmt_client1.add_forwarding_rule(Name("/repo/r1"), [0])
 
-mgmt_client1.add_new_content(Name("/lib/getNext"),"PYTHONSTREAM\ngetNext\ndef getNext(arg):\n    a = get_next(arg)\n    b = get_next(arg)\n    c = get_next(arg)\n    d = get_next(arg)\n    e = get_next(arg)\n    f = get_next(arg)\n    g = get_next(arg)\n    h = get_next(arg)\n    i = get_next(arg)\n    j = get_next(arg)\n    k = get_next(arg)\n    print('K is not on list, k =:', k)\n    return a + b + c + d + e + f + g + h + i + j")
+mgmt_client1.add_new_content(Name("/lib/getNext"),"PYTHONSTREAM\ngetNext\ndef getNext(arg):\n    a = get_next(arg)\n    sleep(2)\n    b = get_next(arg)\n    sleep(2)\n    c = get_next(arg)\n    sleep(2)\n    d = get_next(arg)\n    sleep(2)\n    e = get_next(arg)\n    sleep(2)\n    f = get_next(arg)\n    sleep(2)\n    g = get_next(arg)\n    sleep(2)\n    h = get_next(arg)\n    sleep(2)\n    i = get_next(arg)\n    sleep(2)\n    j = get_next(arg)\n    sleep(2)\n    k = get_next(arg)\n    sleep(2)\n    print('K is not on list, k =:', k)\n    return a + b + c + d + e + f + g + h + i + j")
 
 getNextTest = Name("/lib/getNext")
 getNextTest += '_(/repo/r1/exampleInputFile)'
 getNextTest += "NFN"
 
 start_time = time.perf_counter()
-res = fetch_tool.fetch_data(getNextTest)
+res = fetch_tool.fetch_data(getNextTest, timeout=60)
 end_time = time.perf_counter()
 print("Interest result:", res)
 print("Time needed in seconds:", end_time-start_time)
