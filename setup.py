942fdb94ed57d3fff1ccbe94a185eee657a40885
Luc Kury <luc.kury@gmail.com>
2020-07-09 11:09:11 +0200

    [FIX] Adds corret formatting.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ config_PiCN = {
     'tests_require': ['nose2', 'rednose', 'nose-progressive', 'numpy', 'cv'],
     'name': 'PiCN'
 }
-classifiers_PiCN=[
+classifiers_PiCN = [
     'Programming Language :: Python',
     'Environment :: Console',
     'Network Application :: ICN :: NFN'
